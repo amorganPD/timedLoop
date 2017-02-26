555f59d7d53f605fe8bfdd379874dd4ab04727d3
amorganPD <amorgan@prioritydesigns.com>
2017-02-26 18:54:22 -0500

    Each registered function gets it's own setInterval call

--- dist/timedLoop.0.1.0.js
+++ dist/timedLoop.0.1.0.js
@@ -3,50 +3,59 @@ var timedLoop = (function (timedLoop, undefined) {
 	'use strict';
 
 	// private variables
-	var logicFunctions = [];
-	var loopStart = 0; // Initial Loop Time
-	var loopDeltaTime = 0; // Delta Time of loop
-	var loopTimeDiff = 0; // Difference between LoopTime and dT_Loop
+	var loopFunctions = [];
 	var loopTIME = 50; // Desired loop time in milliseconds
-	var loopWait = 50; // Wait time for setTimeout
-	var loopAverage = 0; // Wait time for setTimeout
-	var actualWait = 0; // time elapsed until setTimeout actual calls function, this takes into account that other functions and actions are happening
-	var waitDifference = 0;
-	var previousTime = 0;
-	var nowTime = 0;
 
-	// exposed variables
-	timedLoop.loopId = 0;
-
-	timedLoop.setLoopTime = function (desiredLoopTime) {
-		// Browser does not allow lower than 4ms delay, check for that condition
-		loopTIME = (desiredLoopTime >= 4) ? desiredLoopTime : 4;
-		loopWait = loopTIME;
-		loopAverage = loopTIME;
-	}
-
-	timedLoop.registerFunction = function (newFunction, newData, priority) {
+	var loopFunction = function (newFunction, name, newData, loopTime, priority) {
 		var NewFunction = {};
 
 		NewFunction.pointer = newFunction;
 		NewFunction.data = newData || {};
 		NewFunction.priority = priority || 1;
+		NewFunction.useCustomLoopTime = (loopTime == undefined) ? false : true;
+		NewFunction.loopTime = loopTime || loopTIME;
+		NewFunction.id = 0;
+		NewFunction.nowTime = performance.now();
+		NewFunction.previousTime = NewFunction.nowTime;
+		NewFunction.loopAverage = NewFunction.loopTime;
 
-		logicFunctions.push(NewFunction);
+		return NewFunction;
+	}
+	// exposed variables
+	timedLoop.loopId = 0;
+
+	timedLoop.setLoopTime = function (desiredLoopTime) {
+		// Browsers cannot guarantee less than 15ms delay
+		if (desiredLoopTime < 15) { 
+			console.log("timedLoop: Cannot set lower than 15ms");
+		}
+		loopTIME = (desiredLoopTime < 15) ? 15 :desiredLoopTime;
+	}
+
+	timedLoop.registerFunction = function (newFunction, name, newData, loopTime, priority) {
+		var index = loopFunctions.push(new loopFunction(newFunction, name, newData, loopTime, priority));
+		loopFunctions[index - 1].name = name || ('timeLoop-' + (index - 1));
+	}
+
+	var loopFunctionSync = function (iLoop) {
+		var thisFunction = loopFunctions[iLoop];
+		
+		thisFunction.id = setInterval(function () {
+			thisFunction.previousTime = thisFunction.nowTime;
+			thisFunction.nowTime = performance.now();
+
+			thisFunction.pointer(thisFunction.data);
+			thisFunction.loopAverage = thisFunction.loopAverage * .9 + .1 * (thisFunction.nowTime - thisFunction.previousTime);
+		}, (thisFunction.useCustomLoopTime) ? thisFunction.loopTime : loopTIME);
+
+		iLoop++;
+		if (iLoop < loopFunctions.length) {
+			loopFunctionSync(iLoop);
+		}
 	}
 
 	timedLoop.start = function () {
-		timedLoop.loopIntervalId = setInterval(function () {
-			previousTime = nowTime;
-			nowTime = performance.now();
-			var timeDiff = nowTime - previousTime;
-
-			loopAverage = loopAverage * .9 + .1 * (timeDiff);
-
-			for (var iLoop = 0; iLoop < logicFunctions.length; iLoop++) {
-				logicFunctions[iLoop].pointer(logicFunctions[iLoop].data);
-			}
-		}, loopTIME);
+		loopFunctionSync(0);
 	}
 
 	timedLoop.stop = function () {
@@ -54,6 +63,11 @@ var timedLoop = (function (timedLoop, undefined) {
 	}
 
 	timedLoop.getLoopTime = function () {
+		var loopAverage = 0;
+		loopFunctions.forEach(function(element) {
+			loopAverage += element.loopAverage;
+		});
+		loopAverage = loopAverage / (loopFunction.length - 1);
 		return loopAverage; // in ms
 	}
 
--- example/example.js
+++ example/example.js
@@ -1,19 +1,31 @@
 
 var init = function() {
-  var targetLoopTime = 50;
+  var targetLoopTime = 1000;
 
   function timeDelay(delayms) {
     var start = Date().now;
     while (Date().now - start < delayms);
   }
-
+  
   function changeHtml() {
     var randomNumber = Math.random()*targetLoopTime;
     $("#textField").html("Random Number: " + randomNumber.toFixed() + "<br />AvgWait: " + timedLoop.getLoopTime().toFixed());
     timeDelay(randomNumber);
   }
+  function testFunction_1() {
+    console.log("1");
+  }
+  function testFunction_2() {
+    console.log("2");
+  }
+  function testFunction_3() {
+    console.log("3");
+  }
   
   timedLoop.registerFunction(changeHtml);
+  timedLoop.registerFunction(testFunction_1, "timedloop-1", {}, 50);
+  timedLoop.registerFunction(testFunction_2);
+  timedLoop.registerFunction(testFunction_3);
   timedLoop.setLoopTime(targetLoopTime);
   timedLoop.start();
   
